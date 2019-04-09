e8d56ffe429997f2f67156636c92a511184743f3
IvelinMarinov <ivelin.marinov@gmail.com>
2019-04-09 23:44:07 +0300

    reegister form and validations

--- epl-betting/src/app/app.module.ts
+++ epl-betting/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true }
--- epl-betting/src/app/components/register/register.component.ts
+++ epl-betting/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,14 +7,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('registerForm')
+  form;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      dateOfBirth: ['', [Validators.required, this.validateDateOfBirth]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{4,15}$')]],
+      repeatPassword: ['', Validators.required]
+    }, {
+        validator: this.passwordsMustMatch()
+      })
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  passwordsMustMatch() {
+    return (formGroup: FormGroup) => {
+      const pass = formGroup.controls['password'];
+      const repeatPass = formGroup.controls['repeatPassword'];
+
+      if (repeatPass.errors && !repeatPass.errors.mustMatch) {
+        return;
+      }
+
+      if (pass.value !== repeatPass.value) {
+        repeatPass.setErrors({ mustMatch: true });
+      } else {
+        repeatPass.setErrors(null);
+      }
+    }
+  }
+
+  validateDateOfBirth(control: AbstractControl) {
+    if(control.errors) {
+      return null;
+    }
+
+    const dobStr = control.value;
+
+    if (!Date.parse(dobStr)) {
+      return { invalidDate: true }
+    }
+
+    let dob = new Date(dobStr);
+    let ageDiffMs = Date.now() - dob.getTime();
+    let ageDate = new Date(ageDiffMs);
+    let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    if(age < 18) {
+      return { underAge: true }
+    }
+
+    return null;
   }
 
   register() {
-    console.log('in')
+    console.log(this.form)
   }
-
 }
